0b329c48299989c84c68420fab51491ce27367d3
mike vallano <mikevallano@gmail.com>
2015-02-23 09:38:27 -0600

    adds greeting to homepage

--- home_splash/app/controllers/home_controller.rb
+++ home_splash/app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
+    right_now = Time.now
+    @current_time = right_now.to_formatted_s(:short)
+
+    # Adjust message by time of day
+    @greet_message = case right_now.hour
+    when 5..11
+      "goooooooood mornin"
+    when 12..15
+      "happy afternooooooon"
+    when 16..19
+      "good evenin' there, fella"
+    when 20..23
+      "good night"
+    when 0..4
+      "hello madrugada"
+    end
+
   end
 end
