fd3d0eb733237a25decee4472291b0924057e713
mike vallano <mikevallano@gmail.com>
2015-03-09 09:10:53 -0500

    generated clearance and migrated

--- home_splash/config/environments/production.rb
+++ home_splash/config/environments/production.rb
@@ -60,6 +60,10 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # TODO Update this when we push to Heroku
+  # Link for emails etc.
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
