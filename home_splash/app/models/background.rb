05630ec399bb87571d32e619d6d316be4e6a50f6
mike vallano <mikevallano@gmail.com>
2015-03-17 08:47:24 -0500

    add validations to models

--- home_splash/app/models/background.rb
+++ home_splash/app/models/background.rb
@@ -1,3 +1,6 @@
 class Background < ActiveRecord::Base
   belongs_to :user
+
+  validates :image_file, presence: true, uniqueness: {scope: :user_id}, length: {in: 2..255}
+  #this means that they are unique by user
 end
--- home_splash/app/models/user.rb
+++ home_splash/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   include Clearance::User
+
   has_one :location
   has_many :purposes
   has_many :backgrounds
+
 end
